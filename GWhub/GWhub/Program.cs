115c8e0e0431e470f32eeec13f0857e9ae6558f0
knigawkl <lukasz.knigawka@outlook.com>
2018-11-11 01:32:35 +0100

    Adding final ver of spec fun

--- GWhub/GWhub/Program.cs
+++ GWhub/GWhub/Program.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 namespace GWhub
 {
     static class Program
-    {
+    {/*
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +16,9 @@ namespace GWhub
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
+        */
+
     }
 }
