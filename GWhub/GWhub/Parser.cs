aeae37f3bb40c4554eae58ad5e9a4552c4b7855b
knigawkl <lukasz.knigawka@outlook.com>
2018-11-27 01:43:57 +0100

    Adding probably the last version of input validation

--- GWhub/GWhub/Parser.cs
+++ GWhub/GWhub/Parser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace GWhub
@@ -46,30 +47,86 @@ namespace GWhub
                     throw new Exception("The 2nd comment line not correct. Expected: # Waluty (id | symbol | pełna nazwa)");
                 }
 
-
                 int secondCommentLine = lines.FindLastIndex(x => x.StartsWith("#"));
                 
                 for (int i = 1; i < secondCommentLine; i++)
                 {                   
                     string[] words = lines[i].Split();
+
+                    for (int j = 2; j < words.Length; j++)
+                    {
+                        if (!words[j].Equals(words[j].ToLower()))
+                        {
+                            throw new Exception("Currency long name has to be all lowercase. Line: " + (i + 1));
+                        }
+                        if (words[j].Any(c => char.IsDigit(c)))
+                        {
+                            throw new Exception("Currency long name cannot contain digits. Line: " + (i + 1));
+                        }
+                    }
+
                     if (words.Length < 3)
                     {
-                        throw new Exception("There are " + words.Length + " elements in line " + i + ". There should be at least 3.");
+                        throw new Exception("There are " + words.Length + " elements in line " + (i + 1) + ". There should be at least 3.");
                     }
                     else if (words[1].Length != 3)
                     {
-                        throw new Exception("Impossible currency symbol at line " + i + ": " + words[1]);
+                        throw new Exception("Impossible currency symbol at line " + (i + 1) + ": " + words[1]);
                     }
                     else if (!words[1].Equals(words[1].ToUpper()))
                     {
-                        throw new Exception("Currency symbol has to be uppercase. Invalid symbol: " + words[1] + ", at line: " + i);
+                        throw new Exception("Currency symbol has to be uppercase. Invalid symbol: " + words[1] + ", at line: " + (i + 1));
                     }
+                    else if (!int.TryParse(words[0], out var res))
+                    {
+                        throw new Exception("IDs have to be integers. Line: " + (i + 1));
+                    }
+                    else if (int.Parse(words[0]) != i - 1)
+                    {
+                        throw new Exception("IDs in input file have to be in ascending order, unique, starting from 0.");
+                    }
+
                     parsed.nodes.Add(new CurrencyVertex(words[1]));
                 }
 
                 for (int i = ++secondCommentLine; i < lines.Count; i++)
                 {
                     string[] words = lines[i].Split();
+                    double rate = 0, fee = 0;
+
+                    if (parsed.nodes.Find(x => x.Symbol.Equals(words[1])) == null)
+                    {
+                        throw new Exception("At line "+ (i + 1) + ": " + "There is no such currency symbol as: " + words[1]);
+                    }
+                    else if (parsed.nodes.Find(x => x.Symbol.Equals(words[2])) == null)
+                    {
+                        throw new Exception("At line " + (i + 1) + ": " + "There is no such currency symbol as: " + words[2]);
+                    }
+                    else if (words.Length < 6)
+                    {
+                        throw new Exception("Not enough words in line: " + (i + 1) + ". There should be 6 words, instead of: " + words.Length);
+                    }
+                    else if (words.Length > 6)
+                    {
+                        throw new Exception("Too many words in line: " + (i + 1) + ". There should be 6 words, instead of: " + words.Length);
+                    }
+                    else if (!double.TryParse(words[3], out rate))
+                    {
+                        throw new Exception("Exchange rate cannot be read from: " + words[3]);
+                    }
+                    else if (!double.TryParse(words[5], out fee))
+                    {
+                        throw new Exception("Exchange fee cannot be read from: " + words[5]);
+                    }
+                    else if (!int.TryParse(words[0], out var res))
+                    {
+                        throw new Exception("IDs have to be integers. Line: " + (i + 1));
+                    }
+                    else if (int.Parse(words[0]) != i - secondCommentLine)
+                    {
+                        throw new Exception("IDs in input file have to be in ascending order, unique, starting from 0. Invalid line: " + (i + 1));
+                    }
+
                     int feeType;
                     if (words[4].Equals("STAŁA")) {
                         feeType = (int)ExchangeEdge.ChargeType.Standing;
@@ -85,9 +142,9 @@ namespace GWhub
 
                     parsed.edges.Add(new ExchangeEdge(parsed.nodes.Find(x => x.Symbol == words[1]), 
                         parsed.nodes.Find(x => x.Symbol == words[2]), 
-                        double.Parse(words[3]),
+                        rate,
                         feeType,
-                        double.Parse(words[5])));
+                        fee));
                 }
             }
             catch (Exception e)
